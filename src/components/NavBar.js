7e82281d87abae08fc3b31ee95f39a87d7558431
invader237 <diego.trivino.fr@gmail.com>
2024-12-08 15:49:18 +0100

    :iphone:(Navbar): enhance design and responsiveness

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,44 @@
 import React from 'react';
-import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import { BottomNavigation, BottomNavigationAction, Paper, useMediaQuery } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import SettingsIcon from '@mui/icons-material/Settings';
+
+// Import CSS (si utilisé)
+import '../styles/NavBar/NavBar.css';
 
 const Navbar = () => {
   const [value, setValue] = React.useState(0);
 
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+  
+  const isExtraSmallScreen = useMediaQuery('(max-width:420px)');
+
   return (
     <Paper
       sx={{
-        position: 'fixed',
-        bottom: 16, // Décalage du bas
-        left: 16, // Décalage gauche
-        right: 16, // Décalage droite
-        borderRadius: 16, // Arrondi des coins
-        boxShadow: 3, // Ombre légère
+        display: 'flex',
+        justifyContent: 'space-around',
+        borderRadius: 16,
+        boxShadow: 9,
+        marginBottom: '8px',
       }}
       elevation={3}
     >
       <BottomNavigation
         value={value}
         onChange={(event, newValue) => setValue(newValue)}
+        showLabels={!isSmallScreen} 
         sx={{
-          borderRadius: 16, // Garde les coins arrondis pour toute la navbar
+          borderRadius: 32,
+          width: '100%',
+          padding: isExtraSmallScreen ? '0 5px' : '0 16px', 
         }}
       >
         <BottomNavigationAction label="Home" icon={<HomeIcon />} />
         <BottomNavigationAction label="Search" icon={<SearchIcon />} />
         <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Notifications" icon={<NotificationsIcon />} />
         <BottomNavigationAction label="Settings" icon={<SettingsIcon />} />
       </BottomNavigation>
     </Paper>
