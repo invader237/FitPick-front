b0affe7a6b24facf55665de394d8f7abc3b51808
e3282u <guigui97457@gmail.com>
2024-12-14 01:52:02 +0100

    :sparkles:(ClothingItems.js):add ClothingItem component to render individual clothing items

---
+++ src/components/ClothingItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/ClothingItem.css';
+
+const ClothingItem = ({ item }) => {
+    return (
+        <div className="clothing-item">
+            <img src={item.image} alt={item.name} className="clothing-image" />
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+        </div>
+    );
+};
+
+export default ClothingItem;
