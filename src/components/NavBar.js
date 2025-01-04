f30aba5c691504f8ea2bd28836d1b245f9b07cda
Diego <67508521+invader237@users.noreply.github.com>
2025-01-04 14:41:27 +0100

    Bugfix/nav bar (#17)
    * :bug:(routes): adjust routes
    
    * :rotating_light:(NavBar.js): remove unused variable
    
    * :fire:(package.json): remove duplicated library
    
    * :coffin:(Navbar): remove unused CSS file for navbar
    
    * :lipstick:(Navbar): fix issues in navbar functionality and design
    
    * :rotating_light:(Layout.js): remove unused import
    
    * :fire:(NavBar.js): clean up UI by removing unused styling
    
    * :lipstick:(UI): correct positioning issue for the add button
    
    * :lipstick:(UI): correct styling problems in navbar
    
    * :lipstick:(UI): fix add button positioning to align with new clothing library frontend
    
    * :bug:(UI): fix add button positioning to align with new clothing library frontend

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,53 +1,70 @@
 import React from 'react';
-
 import { BottomNavigation, BottomNavigationAction, Paper, useMediaQuery } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import Inventory from '@mui/icons-material/Inventory';
 import { Person } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
-import '../styles/NavBar/NavBar.css';
 
 const Navbar = () => {
-  const [value, setValue] = React.useState(0);
-  const isSmallScreen = useMediaQuery('(max-width:600px)');
-  const isExtraSmallScreen = useMediaQuery('(max-width:420px)');
-  const navigate = useNavigate();
+    const [value, setValue] = React.useState(0);
+    const isSmallScreen = useMediaQuery('(max-width:600px)');
+    const isExtraSmallScreen = useMediaQuery('(max-width:420px)');
+    const navigate = useNavigate();
 
-  const handleNavigation = (event, newValue) => {
-    setValue(newValue);
-    const routes = ['/', '/', '/inventory', '/dashboard'];
-    navigate(routes[newValue]);
-  };
+    const handleNavigation = (event, newValue) => {
+        setValue(newValue);
+        const routes = ['/', '/', '/inventory', '/dashboard'];
+        navigate(routes[newValue]);
+    };
 
-  return (
-    <Paper
-      sx={{
+    const navbarItemStyle = {
         display: 'flex',
-        justifyContent: 'space-around',
-        borderRadius: 16,
-        boxShadow: 9,
-        marginBottom: '8px',
-      }}
-      elevation={3}
-    >
-      <BottomNavigation
-        value={value}
-        onChange={handleNavigation}
-        showLabels={!isSmallScreen}
-        sx={{
-          borderRadius: 32,
-          width: '100%',
-          padding: isExtraSmallScreen ? '0 5px' : '0 16px',
-        }}
-      >
-        <BottomNavigationAction label="Accueil" icon={<HomeIcon />} />
-        <BottomNavigationAction label="Recherche" icon={<SearchIcon />} />
-        <BottomNavigationAction label="Inventaire" icon={<Inventory />} /> 
-        <BottomNavigationAction label="Profil" icon={<Person />} />
-      </BottomNavigation>
-    </Paper>
-  );
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '0.5rem 1rem',
+        borderRadius: '5rem',
+        cursor: 'pointer',
+        transition: 'all 0.07s ease-in-out',
+    };
+
+    return (
+        <Paper
+            sx={{
+                display: 'flex',
+                flex: 1,
+                position: 'fixed',
+                alignItems: 'center',
+                bottom: 0,
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                borderRadius: 16,
+                boxShadow: 9,
+                marginBottom: '8px',
+                width: 'calc(100% - 16px)',
+                maxWidth: '1080px',
+                zIndex: 1000,
+            }}
+            elevation={3}
+        >
+            <BottomNavigation
+                value={value}
+                onChange={handleNavigation}
+                showLabels={!isSmallScreen}
+                sx={{
+                    borderRadius: 32,
+                    width: '100%',
+                    padding: isExtraSmallScreen ? '0 5px' : '0 16px',
+                }}
+            >
+                <BottomNavigationAction sx={navbarItemStyle} label="Accueil" icon={<HomeIcon />} />
+                <BottomNavigationAction sx={navbarItemStyle} label="Recherche" icon={<SearchIcon />} />
+                <BottomNavigationAction sx={navbarItemStyle} label="Inventaire" icon={<Inventory />} />
+                <BottomNavigationAction sx={navbarItemStyle} label="Profil" icon={<Person />} />
+            </BottomNavigation>
+        </Paper>
+    );
 };
 
 export default Navbar;
