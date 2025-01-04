8a000b8b34a7c99fc389ac63d6596d50382c966e
e3282u <guigui97457@gmail.com>
2025-01-04 16:50:22 +0100

    :wrench:(Layout.js): Update layout to include navigation to inventory

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -27,7 +27,7 @@ const Layout = () => {
   const isWeatherPage = location.pathname === '/';
 
   return (
-    <div className={`main-container ${isWeatherPage ? 'weather-page-bg' : "" }`}>
+    <div className={`main-container ${isWeatherPage ? 'weather-page-bg' : ""}`}>
 
       <Routes>
         {/* Routes d'authentification */}
