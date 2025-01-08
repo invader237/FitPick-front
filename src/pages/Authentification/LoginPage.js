6ba3091bd6236a003b90d4ba1aef4f1951a92e30
HyMathew <mathieu.drissi@outlook.fr>
2025-01-08 07:36:27 +0100

    ✨ (LoginPage.js): update redirect after successful login to navigate to home page

--- src/pages/Authentification/LoginPage.js
+++ src/pages/Authentification/LoginPage.js
@@ -48,7 +48,7 @@ const LoginPage = () => {
       localStorage.setItem("authToken", response.data.token);
       setSuccess("Connexion réussie ! Redirection...");
       setShowAlert(true);
-      setTimeout(() => navigate("/dashboard"), 2000);
+      setTimeout(() => navigate("/"), 2000);
     } catch (err) {
       setError("Email ou mot de passe incorrect.");
       setShowAlert(true);
